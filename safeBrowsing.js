6087347955141c4e61bbd7855d64c299d52cd931
goodsites-rda <mr.gamer3049@gmail.com>
2025-04-23 12:46:08 +0200

    Create safeBrowsing.js

---
+++ safeBrowsing.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const API_KEY = 'TU_API_KEY_GOOGLE_SAFE_BROWSING';
+
+exports.checkUrlSafety = async (url) => {
+  try {
+    const res = await axios.post(
+      `https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${API_KEY}`,
+      {
+        client: { clientId: 'qr-checker', clientVersion: '1.0' },
+        threatInfo: {
+          threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING'],
+          platformTypes: ['ANY_PLATFORM'],
+          threatEntryTypes: ['URL'],
+          threatEntries: [{ url }],
+        },
+      }
+    );
+    return !res.data.matches;
+  } catch (err) {
+    return false;
+  }
+};
